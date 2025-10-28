db3f7cffeb6592cd13232b48202f584d6574f007
Juan Miguel Gutierrez V. <munekoporsiempre2017@gmail.com>
2025-10-28 11:52:42 -0500

    Update Equipo.java
    se realizo el crud para jugador en equipo y se agrego un metodo para saber si todos los jugadores del equipo estan derrotados

--- src/main/java/co/edu/uniquindio/poo/juego/model/Equipo.java
+++ src/main/java/co/edu/uniquindio/poo/juego/model/Equipo.java
@@ -2,6 +2,7 @@ package co.edu.uniquindio.poo.juego.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class Equipo {
     private String nombre;
@@ -29,5 +30,33 @@ public class Equipo {
 
     public void setJugadores(List<Jugador> jugadores) {
         this.jugadores = jugadores;
+    }
+
+    public void addJugador(Jugador jugador) {
+        Optional<Jugador> encontrado = buscarJugador(jugador.getNombre());
+        if(encontrado.isPresent()) {
+            throw new IllegalArgumentException("El jugador ya existe");
+        } else {
+            this.jugadores.add(jugador);
+        }
+    }
+    public Optional<Jugador> buscarJugador(String nombre){
+        return jugadores.stream().filter(j -> j.getNombre().equals(nombre)).findFirst();
+    }
+    public void deleteJugador(String nombre){
+        Optional<Jugador> encontrado = buscarJugador(nombre);
+        if(encontrado.isPresent()) {
+            jugadores.remove(encontrado.get());
+        } else {
+            throw new IllegalArgumentException("El jugador no existe");
+        }
+    }
+    public boolean todosDerrotados(){
+        for (Jugador j : jugadores) {
+            if (j.isVivo()) {
+                return false;
+            }
+        }
+        return true;
     }
 }
